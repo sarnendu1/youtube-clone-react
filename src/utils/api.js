9fabafe1c9c89cdc1b70e14340e598b2b9cc1ee1
Sarnendu Das <sarnendu1@gmail.com>
2023-08-18 20:48:11 +0530

    updated code

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
-        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+        'X-RapidAPI-Key': 'fafd1d178dmshd425bdbf538ec89p12fc03jsnc0b05368463c',
         'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
       }
 };
